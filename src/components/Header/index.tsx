e04559727b9249962fa7140073f5d020df665c50
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2026-02-13 16:06:33 -0300

    feat: implement Header component with styles and update App layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,31 @@
-const Header = () => <header>Cabeçalho</header>
+import { HeaderBar, LinkItem, Links, LinkCart } from './styles'
+
+import logo from '../../assets/images/logo.svg'
+import carrinho from '../../assets/images/carrinho.svg'
+
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="Logo EPLAY" />
+      <nav>
+        <Links>
+          <LinkItem>
+            <a href="#">Categorias</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Novidades</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Promoções</a>
+          </LinkItem>
+        </Links>
+      </nav>
+    </div>
+    <LinkCart href="#">
+      0 - produto(s)
+      <img src={carrinho} alt="Carrinho de compras" />
+    </LinkCart>
+  </HeaderBar>
+)
 
 export default Header
